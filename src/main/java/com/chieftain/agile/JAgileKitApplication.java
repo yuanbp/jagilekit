6e4dd2791a56a214ce5b35f44cea11d16fda8890
yuanbaiping <yuanbp0912@163.com>
2019-01-03 23:12:04 +0800

    结构调整

--- src/main/java/com/chieftain/agile/JAgileKitApplication.java
+++ src/main/java/com/chieftain/agile/JAgileKitApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
@@ -13,7 +12,6 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @ServletComponentScan
 @EnableWebMvc
 @EnableTransactionManagement
-@ComponentScan(basePackages = "com.chieftain.agile")
 @EnableCaching
 @MapperScan(basePackages = "com.chieftain.agile.dao")
 public class JAgileKitApplication {
---
+++ src/main/java/com/chieftain/agile/common/webmvc/Swagger2Config.java
@@ -0,0 +1,43 @@
+package com.chieftain.agile.common.webmvc;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * com.chieftain.agile [workset]
+ * Created by chieftain on 2018/9/6
+ *
+ * @author chieftain on 2018/9/6
+ */
+@Configuration
+@EnableSwagger2
+public class Swagger2Config {
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.chieftain.agile.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Spring Boot中使用Swagger2构建RESTful APIs")
+                .description("spring Boot Swagger2")
+                .termsOfServiceUrl("")
+                .contact("chieftain")
+                .version("1.0")
+                .build();
+    }
+}
